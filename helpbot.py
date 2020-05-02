b503ff86372f86c0781b0c238e704160c69cfb00
Chris Archibald <carchi8py@gmail.com>
2020-05-02 16:11:22 -0700

    read a channels history

--- helpbot.py
+++ helpbot.py
@@ -10,6 +10,7 @@ class HelpBot(object):
         # TODO: Might want to be able to auto join the channel
         self.mychannel = 'carchitest2'
         self.slack_client = slack.WebClient(token=token)
+        self.channel_id = self.get_channel_id()
 
     def post_message(self, message):
         self.slack_client.chat_postMessage(
@@ -17,8 +18,24 @@ class HelpBot(object):
             text=message
         )
 
+    def get_channel_id(self):
+        list = self.slack_client.channels_list()
+        for each in list.data.get('channels'):
+            if each.get('name') == self.mychannel:
+                return each.get('id')
+        # TODO: If we can't get the channel id, we should quit
+        return None
+
+    def get_channel_history(self):
+        return self.slack_client.conversations_history(channel=self.channel_id)
+
     def run(self):
         self.post_message("Test message")
+        history = self.get_channel_history()
+        if history['messages']:
+            for message in history['messages']:
+                if message['text']:
+                    print(str(message['text']))
 
 
 def main():

12d8f63ccebba26f8c60e631e7570ba0366c23eb
Chris Archibald <carchi8py@gmail.com>
2020-05-02 21:45:38 -0700

    start a new thread when a specific phrase is used

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ This is a project to create a simple help bot in slack for the ansible module i 
 ## Versions
 * 0.1 Write a message to Slack channel
 * 0.2 Read Slack message history
+* 0.3 Start a new thread if a keyword is found
--- helpbot.py
+++ helpbot.py
@@ -6,17 +6,27 @@ from mytoken import token
 
 class HelpBot(object):
     def __init__(self):
-        logging.basicConfig()
+        logging.basicConfig(level=logging.INFO)
         # TODO: Might want to be able to auto join the channel
         self.mychannel = 'carchitest2'
         self.slack_client = slack.WebClient(token=token)
+        logging.info("Start up")
         self.channel_id = self.get_channel_id()
 
-    def post_message(self, message):
-        self.slack_client.chat_postMessage(
-            channel='#'+self.mychannel,
-            text=message
-        )
+    def post_message(self, message, thread_ts=None):
+        # Post a message as a thread to a message
+        if thread_ts is not None:
+            self.slack_client.chat_postMessage(
+                channel='#' + self.mychannel,
+                text=message,
+                thread_ts=thread_ts
+            )
+        # Post a message as a normal message
+        else:
+            self.slack_client.chat_postMessage(
+                channel='#'+self.mychannel,
+                text=message
+            )
 
     def get_channel_id(self):
         list = self.slack_client.channels_list()
@@ -26,16 +36,29 @@ class HelpBot(object):
         # TODO: If we can't get the channel id, we should quit
         return None
 
-    def get_channel_history(self):
-        return self.slack_client.conversations_history(channel=self.channel_id)
+    def get_channel_history(self, oldest=None):
+        if oldest is not None:
+            return self.slack_client.conversations_history(channel=self.channel_id, oldest=str(oldest))
+        else:
+            return self.slack_client.conversations_history(channel=self.channel_id)
+
+    def parse_message(self, message):
+        if message['text']:
+            message_text = message['text']
+            if '404' in message_text:
+                self.post_message('You Failed', thread_ts=message['ts'])
 
     def run(self):
-        self.post_message("Test message")
-        history = self.get_channel_history()
-        if history['messages']:
-            for message in history['messages']:
-                if message['text']:
-                    print(str(message['text']))
+        now = time.time()
+        while True:
+            history = self.get_channel_history(now)
+            logging.info(str(history))
+            now = time.time()
+            # If there are message we want to do something
+            if history['messages']:
+                for message in history['messages']:
+                    self.parse_message(message)
+            time.sleep(10)
 
 
 def main():
